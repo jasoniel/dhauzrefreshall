61f9befec73c723065f9159923113fa7af8ba6f6
Jasoniel Santana <jasoniel.santana@dhauz.com>
2025-02-14 14:05:15 -0300

    add read P_REFRESH_FREQUENCY tableau parameter to frequency update definition

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 let refreshInterval;
 let isRefreshing = false;
 let worksheet;
+let TIME_PERIOD = 5 //MINUTES
+let TIME_PERIOD_DEFINITION =  300000; //CONVERT IN MILLISECONDS
 
 // Inicializar a extensão
 tableau.extensions.initializeAsync().then(() => {
     // document.getElementById('status').textContent = "Conectado";
     document.getElementById('toggleRefresh').textContent = "Parar Auto-Refresh";
+    const dashboard = tableau.extensions.dashboardContent.dashboard;   
+
+    dashboard.findParameterAsync("P_REFRESH_FREQUENCY").then(function(param){ 
+        TIME_PERIOD = parseFloat(param.currentValue.value) || 5;
+        TIME_PERIOD_DEFINITION = (TIME_PERIOD == 0 ? 5 : TIME_PERIOD) * 60000;
+        document.getElementById('time').textContent = TIME_PERIOD_DEFINITION;
+    }).catch((err) => {
+        TIME_PERIOD_DEFINITION = TIME_PERIOD * 60000;
+        document.getElementById('time').textContent = TIME_PERIOD_DEFINITION;
+
+      });     
     initWorksheet();
 });
 
 // Configurar a worksheet
 async function initWorksheet() {
-    const dashboard = tableau.extensions.dashboardContent.dashboard;    
-    document.getElementById('toggleRefresh').addEventListener('click', toggleRefresh);
+    
+    document.getElementById('toggleRefresh').addEventListener('click', toggleRefresh)
     toggleRefresh();
 }
 function refreshAllDataSources() {
@@ -39,14 +52,14 @@ function refreshAllDataSources() {
 function toggleRefresh() {
     if (!isRefreshing) {
         // Iniciar intervalo
-        refreshInterval = setInterval(refreshAllDataSources, 300000); // 5 minutos
-
+        refreshInterval = setInterval(refreshAllDataSources, TIME_PERIOD_DEFINITION); // 5 minutos
         document.getElementById('toggleRefresh').textContent = "Parar Auto-Refresh";
         isRefreshing = true;
     } else {
         // Parar intervalo
         clearInterval(refreshInterval);
         document.getElementById('toggleRefresh').textContent = "Iniciar Auto-Refresh";
+
         isRefreshing = false;
     }
 }
